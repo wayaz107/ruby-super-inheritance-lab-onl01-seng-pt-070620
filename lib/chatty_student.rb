2605ba4b097d1f5521cb5b33f18cf03ea2f268e3
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 07:14:30 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,16 +6,9 @@ puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch Th
 end
 
 def raise_hand
-super
-super
-super
-super
-super
-super
-super
-super
-super
-super
+10.times do
+  super
+end
 end
 
 
